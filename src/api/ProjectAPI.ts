dc43bf374cf11a6e9456b00842952bf5e88340e1
notlim_dev <elmotigre6@gmail.com>
2024-10-24 23:40:27 -0500

    Implementacion manejo de respuestas  react-query
    => Utilizacion de useMutation para consultar la funcion del endpoint con
    axios y el manejo de repsuestas como success y error, predefinidos en el
    backend y axios.

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,6 @@
 import api from "@/lib/axios";
 import { ProjectFormData } from "@/types/index";
+import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
 
@@ -7,7 +8,9 @@ export async function createProject(formData: ProjectFormData) {
     const { data } = await api.post('/projects', formData)
     return data
   } catch (error) {
-    console.log(error);
 
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
   }
 }
