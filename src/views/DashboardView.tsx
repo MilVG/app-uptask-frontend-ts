7490955270c996532e53dd5fe2875b13897e85a3
notlim_dev <elmotigre6@gmail.com>
2024-10-25 12:16:27 -0500

    Finalizacion de capitulo 3 frontend proyectos

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,6 +1,48 @@
+import { Link } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getProjects } from "@/api/ProjectAPI"
+import DetailsProject from "@/components/projects/DetailsProject"
 export default function DashboardView() {
-  return (
-    <div>DashboardView</div>
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['projects'],
+    queryFn: getProjects
+  })
+
+  if (isLoading) return 'cargando...'
+
+  if (data) return (
+    <>
+      <h1 className="text-5xl font-black">Mis proyectos</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">
+        Maneja y Administra tus proyectos
+      </p>
+
+      <nav className="my-5">
+        <Link
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl
+          font-bold cursor-pointer transition-colors"
+          to='/projects/create'
+        >Nuevo Proyecto</Link>
+      </nav>
+
+      {data.length ? (
+        data.map(project => (
+          <DetailsProject
+            key={project._id}
+            project={project}
+          />
+        ))
+      ) : (
+        <>
+          <p className="text-center py-20">No hay proyectos aún {''}</p>
+          <Link
+            to='/projects/create'
+            className="text-fuchsia-500 font-bold"
+          >Crear proyectos</Link>
+        </>
+      )}
+    </>
   )
 }
 
