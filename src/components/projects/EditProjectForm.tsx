fd9aee2e07a649e9fbe55aea349aaa7c70fc606f
notlim_dev <elmotigre6@gmail.com>
2024-10-26 12:32:32 -0500

    Inavalidar cache al actulziar proyecto con tanstack/react-query

--- src/components/projects/EditProjectForm.tsx
+++ src/components/projects/EditProjectForm.tsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import ProjectForm from "./ProjectForm";
 import { Project, ProjectFormData } from "@/types/index";
 import { useForm } from "react-hook-form";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateProject } from "@/api/ProjectAPI";
 import { toast } from "react-toastify";
 
@@ -17,15 +17,22 @@ export default function EditProjectForm({ data, projectId }: DataProjectProps) {
     clientName: data.clientName,
     description: data.description
   }
+  const navigate = useNavigate()
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-  const navigate = useNavigate()
+  const queryClient = useQueryClient()
   const { mutate } = useMutation({
     mutationFn: updateProject,
     onError: (error) => {
       toast.error(error.message)
     },
     onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ['projects']
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['editProject', projectId]
+      })
       toast.success(data.msg)
       navigate('/')
     }
