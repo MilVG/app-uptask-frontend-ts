41173ea8d19faa2c704e14d6ae99082488f56cb0
notlim_dev <elmotigre6@gmail.com>
2024-10-27 17:33:59 -0500

    Incorporacion Formulario y envio de propos de useForm

--- src/components/tasks/AddTaskModal.tsx
+++ src/components/tasks/AddTaskModal.tsx
@@ -1,60 +1,91 @@
 import { Fragment } from 'react';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { TaskFormData } from '@/types/index';
+import { useForm } from 'react-hook-form';
+import TaskForm from './TaskForm';
 
 export default function AddTaskModal() {
-  
+
   const navigate = useNavigate()
   const location = useLocation()
   const queryParams = new URLSearchParams(location.search)
   const modalTask = queryParams.get('newTask')
   const show = modalTask ? true : false
-  
-    return (
-        <>
-            <Transition appear show={show} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={() => navigate(location.pathname,{replace:true})}>
-                    <TransitionChild
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black/60" />
-                    </TransitionChild>
 
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <TransitionChild
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                                    <DialogTitle
-                                        as="h3"
-                                        className="font-black text-4xl  my-5"
-                                    >
-                                        Nueva Tarea
-                                    </DialogTitle>
+  const initialValues: TaskFormData= {
+    name: '',
+    description:''
+  }
 
-                                    <p className="text-xl font-bold">Llena el formulario y crea  {''}
-                                        <span className="text-fuchsia-600">una tarea</span>
-                                    </p>
+  const {register,handleSubmit,formState:{errors}} = useForm({defaultValues:initialValues})
 
-                                </DialogPanel>
-                            </TransitionChild>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </>
-    )
+  const handleCreateTask  = (formData:TaskFormData) => {
+    console.log(formData);
+    
+  }
+  return (
+    <>
+      <Transition appear show={show} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => navigate(location.pathname, { replace: true })}>
+          <TransitionChild
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/60" />
+          </TransitionChild>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <TransitionChild
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                  <DialogTitle
+                    as="h3"
+                    className="font-black text-4xl  my-5"
+                  >
+                    Nueva Tarea
+                  </DialogTitle>
+
+                  <p className="text-xl font-bold">Llena el formulario y crea  {''}
+                    <span className="text-fuchsia-600">una tarea</span>
+                  </p>
+
+                  <form
+                    className='mt-10 space-y-3'
+                    noValidate
+                    onSubmit={handleSubmit(handleCreateTask)}
+                  >
+                    
+                    <TaskForm
+                      register={register}
+                      errors={errors}
+                    />
+
+                    <input
+                      type="submit"
+                      value="Guardar Cambios"
+                      className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                    />
+                  </form>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
 }
