7003409fadf9a04cac6e4b9651d1739102eafb41
notlim_dev <elmotigre6@gmail.com>
2024-10-28 16:52:35 -0500

    Incorporando colores para cada estado de la tarea

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -14,7 +14,13 @@ const initialStatusGroups: GroupTasks = {
   underReview: [],
   completed: [],
 }
-
+const statusStyles: { [key: string]: string } = {
+  pending: 'border-t-slate-500',
+  onHold: 'border-t-red-500',
+  inProgress: 'border-t-blue-500',
+  underReview: 'border-t-amber-500',
+  completed: 'border-t-emerald-500',
+}
 const statusTraslations: { [key: string]: string } = {
   pending: 'Pendiente',
   onHold: 'En Espera',
@@ -38,7 +44,7 @@ export default function TaskList({ tasks }: TaskListProps) {
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
             <h3
               className={`capitalize text-xl font-light border border-slate-300
-                bg-white p-3 border-t-8`}
+                bg-white p-3 border-t-8 ${statusStyles[status]}`}
             >{statusTraslations[status]}</h3>
             <ul className='mt-5 space-y-5'>
               {tasks.length === 0 ? (
