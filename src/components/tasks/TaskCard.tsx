13892f9db4cb4d0c11e0c3b952c20c4bf095b624
notlim_dev <elmotigre6@gmail.com>
2024-10-29 09:02:18 -0500

    crear mutacion para eliminar tareas - tanstack-react-query

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -2,13 +2,31 @@ import { Task } from "@/types/index"
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import { Fragment } from "react/jsx-runtime";
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteTask } from "@/api/taskAPI";
+import { toast } from "react-toastify";
 type TaskCardProps = {
   task: Task
 }
 export default function TaskCard({ task }: TaskCardProps) {
 
   const navigate = useNavigate()
+  const params = useParams()
+
+  const projectId = params.projectId!
+
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: deleteTask,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['project', projectId] })
+      toast.success(data.msg)
+    }
+  })
   return (
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
       <div className="min-w-0 flex flex-col gap-y-4">
@@ -48,7 +66,11 @@ export default function TaskCard({ task }: TaskCardProps) {
               </MenuItem>
 
               <MenuItem>
-                <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500'>
+                <button
+                  type='button'
+                  className='block px-3 py-1 text-sm leading-6 text-red-500'
+                  onClick={() => mutate({ projectId, taskId: task._id })}
+                >
                   Eliminar Tarea
                 </button>
               </MenuItem>
