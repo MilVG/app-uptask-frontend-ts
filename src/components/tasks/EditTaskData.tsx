4adc8c6efd0aa140c23561e44711ad1558d9071b
notlim_dev <elmotigre6@gmail.com>
2024-10-29 08:35:26 -0500

    Envio id Props Modal - incorporacionde errores.

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,6 +1,6 @@
 import { getTaskById } from "@/api/taskAPI"
 import { useQuery } from "@tanstack/react-query"
-import { useLocation, useParams } from "react-router-dom"
+import { Navigate, useLocation, useParams } from "react-router-dom"
 import EditTaskModal from "./EditTaskModal"
 
 
@@ -14,12 +14,14 @@ export default function EditTaskData() {
   const queryParams = new URLSearchParams(location.search)
   const taskId = queryParams.get('editTask')!
 
-  const { data } = useQuery({
+  const { data, isError } = useQuery({
     queryKey: ['task', taskId],
     queryFn: () => getTaskById({ projectId, taskId }),
-    enabled: !!taskId
+    enabled: !!taskId,
+    retry: false
   })
+  if (isError) return <Navigate to={'/404'} />
 
-  if (data) return <EditTaskModal data={data} />
+  if (data) return <EditTaskModal data={data} taskId={taskId} />
 }
 
