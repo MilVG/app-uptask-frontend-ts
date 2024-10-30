3fe8a7e11e3d58292386da9080fa80d309f04f79
notlim_dev <elmotigre6@gmail.com>
2024-10-29 20:11:11 -0500

    Incorporamos los tipados para las fechas de las tareas

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,14 +3,15 @@ import { z } from 'zod'
 /** Tasks */
 
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProcess", "underReview", "completed"])
+export type TaskStatus = z.infer<typeof taskStatusSchema>
 export const taskSchema = z.object({
   _id: z.string(),
   name: z.string(),
   description: z.string(),
   project: z.string(),
   status: taskStatusSchema,
-  createdAt:z.string(),
-  updatedAt:z.string()
+  createdAt: z.string(),
+  updatedAt: z.string()
 })
 
 export type Task = z.infer<typeof taskSchema>
