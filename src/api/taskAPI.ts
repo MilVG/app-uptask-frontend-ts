18042c9e13fd93045040162c30907223635e8bdd
notlim_dev <elmotigre6@gmail.com>
2024-10-30 19:43:16 -0500

    Incorporando y modificando  el manejo de errores de axios  al pedir una tarea especifica.

--- src/api/taskAPI.ts
+++ src/api/taskAPI.ts
@@ -6,6 +6,7 @@ type TaskAPI = {
   formData: TaskFormData
   projectId: Project['_id']
   taskId: Task['_id']
+  status: Task['status']
 }
 export async function createTask({ formData, projectId }: Pick<TaskAPI, 'formData' | 'projectId'>) {
 
@@ -22,18 +23,42 @@ export async function createTask({ formData, projectId }: Pick<TaskAPI, 'formDat
   }
 }
 
+interface ValidationError {
+  type: string;
+  value: string;
+  msg: string;
+  path: string;
+  location: string;
+}
+interface ErrorResponse {
+  errors: ValidationError[];
+}
 export async function getTaskById({ projectId, taskId }: Pick<TaskAPI, 'projectId' | 'taskId'>) {
   try {
     const url = `/projects/${projectId}/tasks/${taskId}`
     const { data } = await api.get(url)
     const response = taskSchema.safeParse(data)
-    if(response){
+    if (response) {
       return response.data
     }
   } catch (error) {
 
     if (isAxiosError(error) && error.response) {
-      throw new Error('Error keys not Accepted ')
+
+      const errorData = error.response.data as ErrorResponse
+      if (errorData.errors && Array.isArray(errorData.errors)) {
+
+        const errorMessage = errorData.errors.map(err => err.msg).join(', ')
+        throw new Error(errorMessage)
+
+      } else if (error.response.data.error) {
+        throw new Error(error.response.data.error)
+      } else {
+        throw new Error("Error de Respuesta de Servidor")
+      }
+    } else {
+
+      throw new Error("errror Desconocido al obtener la tarea")
     }
   }
 }
@@ -43,10 +68,10 @@ export async function updateTask({ projectId, taskId, formData }: Pick<TaskAPI, 
     const { data } = await api.put(url, formData)
     return data
   } catch (error) {
-    
+
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
-      
+
     }
   }
 }
@@ -58,6 +83,21 @@ export async function deleteTask({ projectId, taskId }: Pick<TaskAPI, 'projectId
   } catch (error) {
 
     if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function updateStatus({ projectId, taskId, status }: Pick<TaskAPI, 'projectId' | 'taskId' | 'status'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/status`
+    const { data } = await api.patch(url, { status })
+    return data
+  } catch (error) {
+
+    if (isAxiosError(error) && error.response) {
+      console.log(error.response.data.error);
+
       throw new Error(error.response.data.error)
     }
   }
