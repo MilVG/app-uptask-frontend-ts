e66ed36162fd8c156ac87f8991ae9ea1e5410366
notlim_dev <elmotigre6@gmail.com>
2024-11-02 16:27:40 -0500

    Incorporacion de rutas de Autenticación

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,8 @@ import DashboardView from "@/views/DashboardView";
 import CreateProjectView from "@/views/CreateProjectView";
 import EditProjectView from "@/views/EditProjectView";
 import ProjectDetailsView from "@/views/ProjectDetailsView";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginView from "./views/auth/LoginView";
 
 export default function Router() {
   return (
@@ -12,8 +14,12 @@ export default function Router() {
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashboardView />} index />
           <Route path="/projects/create" element={<CreateProjectView />} index />
-          <Route path="/projects/:projectId" element={<ProjectDetailsView/>} index />
-          <Route path="/projects/:projectId/edit" element={<EditProjectView/>} index />
+          <Route path="/projects/:projectId" element={<ProjectDetailsView />} index />
+          <Route path="/projects/:projectId/edit" element={<EditProjectView />} index />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />} />
         </Route>
       </Routes>
     </BrowserRouter>
