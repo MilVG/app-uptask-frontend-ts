66761fc3f7a1bbd4f22e5fb340fca7fc8ea9fbd7
notlim_dev <elmotigre6@gmail.com>
2024-11-03 22:47:24 -0500

    Definicion e Incorporacion de rutas y vistas para restablecer password

--- src/router.tsx
+++ src/router.tsx
@@ -9,6 +9,8 @@ import LoginView from "./views/auth/LoginView";
 import RegisterView from "./views/auth/RegisterView";
 import ConfirmAccountView from "./views/auth/ConfirmAccountView.tsx"
 import RequestNewCodeView from "./views/auth/RequestNewCodeView.tsx";
+import ForgotPasswordView from "./views/auth/ForgotPasswordView.tsx";
+import NewPasswordView from "./views/auth/NewPasswordView.tsx";
 
 export default function Router() {
   return (
@@ -26,6 +28,8 @@ export default function Router() {
           <Route path="/auth/register" element={<RegisterView />} />
           <Route path="/auth/confirm-account" element={<ConfirmAccountView />} />
           <Route path="/auth/request-code" element={<RequestNewCodeView />} />
+          <Route path="/auth/forgot-password" element={<ForgotPasswordView />} />
+          <Route path="/auth/new-password" element={<NewPasswordView />} />
         </Route>
       </Routes>
     </BrowserRouter>
