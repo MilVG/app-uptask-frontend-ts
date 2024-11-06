cbfb7ccc60505755f3853001814985e4ce054053
notlim_dev <elmotigre6@gmail.com>
2024-11-06 13:15:58 -0500

    Enviamos los datos del usuario via props

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -22,7 +22,9 @@ export default function AppLayout() {
               <Logo />
             </Link>
           </div>
-          <NavMenu />
+          <NavMenu
+            name={data?.name}
+          />
         </div>
       </header>
 
