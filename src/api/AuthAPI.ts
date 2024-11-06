b75b04fdabaa36fc832dee13f8b7292ff6ac01d9
notlim_dev <elmotigre6@gmail.com>
2024-11-06 12:46:48 -0500

    Creamos la peticion a la ruta de user authenticado

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -83,3 +83,15 @@ export async function updatePasswordWithToken({ formData, token }: { formData: N
     }
   }
 }
+
+export async function getuser() {
+  try {
+    const url = '/auth/user'
+    const { data } = await api(url)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
