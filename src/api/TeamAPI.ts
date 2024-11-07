f98fbc5fe1d48b480ffec0c12fedf3b31a1e4cb6
notlim_dev <elmotigre6@gmail.com>
2024-11-07 17:28:50 -0500

    Establecer servicio para obtener los miembros del proyecto

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, TeamMember, TeamMemberForm } from "../types";
+import { Project, TeamMember, TeamMemberForm, TeamMembersSchema } from "../types";
 
 export async function findUserByEmail({ projectId, formData }: { projectId: Project['_id'], formData: TeamMemberForm }) {
   try {
@@ -18,6 +18,20 @@ export async function addUserToProject({ projectId, id }: { projectId: Project['
     const url = `/projects/${projectId}/team`
     const response = await api.post(url, { id })
     return response.data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+export async function getProjectTeam(projectId: Project['_id']) {
+  try {
+    const url = `/projects/${projectId}/team`
+    const { data } = await api(url)
+    const response = TeamMembersSchema.safeParse(data)
+    if (response) {
+      return response.data
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
