0ba7c92e39e6eb9b337a4109448e82d4c7d48433
notlim_dev <elmotigre6@gmail.com>
2024-11-07 15:43:31 -0500

    Modificancion de respuesta de datos del servicio

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -5,10 +5,8 @@ import { Project, TeamMemberForm } from "../types";
 export async function findUserByEmail({ projectId, formData }: { projectId: Project['_id'], formData: TeamMemberForm }) {
   try {
     const url = `/projects/${projectId}/team/find`
-    const { data } = await api.post(url, formData)
-    console.log(data);
-    return data
-
+    const response = await api.post(url, formData)
+    return response.data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
