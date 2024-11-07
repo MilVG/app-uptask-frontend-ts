7ebee24bc4fa07af7ff156f48086aad38463a95b
notlim_dev <elmotigre6@gmail.com>
2024-11-07 18:00:17 -0500

    establecer servicio para remover un miembro de equipo

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -24,6 +24,18 @@ export async function addUserToProject({ projectId, id }: { projectId: Project['
     }
   }
 }
+export async function removeUserFromProject({ projectId, userId }: { projectId: Project['_id'], userId: TeamMember['_id'] }) {
+  try {
+    const url = `/projects/${projectId}/team/${userId}`
+    const response = await api.delete(url)
+    return response.data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
 export async function getProjectTeam(projectId: Project['_id']) {
   try {
     const url = `/projects/${projectId}/team`
