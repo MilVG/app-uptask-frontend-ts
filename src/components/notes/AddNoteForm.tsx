ad3f843d0c7afff0de72008c53f47b90644d0fb2
notlim_dev <elmotigre6@gmail.com>
2024-11-09 18:35:34 -0500

    Incorporaacion e Implementacion de mutacion para crear una nota

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,18 +1,44 @@
 import { NoteFormData } from "@/types/index"
 import { useForm } from "react-hook-form"
 import ErrorMessage from "../ErrorMenssage"
+import { useMutation } from "@tanstack/react-query"
+import { createNote } from "@/api/NoteAPI"
+import { toast } from "react-toastify"
+import { useLocation, useParams } from "react-router-dom"
 
 export default function AddNoteForm() {
 
+  const params = useParams()
+  const location = useLocation()
+
+  const queryParams = new URLSearchParams(location.search)
+
+  const projectId = params.projectId!
+  const taskId = queryParams.get('viewTask')!
   const initialValues: NoteFormData = {
     content: ''
   }
 
-  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+  const { register, reset, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+  const { mutate } = useMutation({
+    mutationFn: createNote,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data.msg)
+      reset()
+    }
+  })
 
   const handleAddNote = (formData: NoteFormData) => {
-    console.log(formData);
-
+    const data = {
+      projectId,
+      taskId,
+      formData
+    }
+    mutate(data)
   }
   return (
     <form
