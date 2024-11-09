087bcb1c0acdf2002a7820c14313af6dde73e158
notlim_dev <elmotigre6@gmail.com>
2024-11-09 18:13:46 -0500

    Agregando el hook useForm y definiendo los parametros y funcionaes que se utilizará

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,7 +1,22 @@
+import { NoteFormData } from "@/types/index"
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../ErrorMenssage"
+
 export default function AddNoteForm() {
+
+  const initialValues: NoteFormData = {
+    content: ''
+  }
+
+  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+  const handleAddNote = (formData: NoteFormData) => {
+    console.log(formData);
+
+  }
   return (
     <form
-      onSubmit={() => { }}
+      onSubmit={handleSubmit(handleAddNote)}
       className=""
       noValidate
     >
@@ -12,7 +27,13 @@ export default function AddNoteForm() {
           type="text"
           placeholder="Contenido de la nota"
           className="w-full p-3 border border-gray-300"
+          {...register('content', {
+            required: 'El contenido de la nota es obligatorio'
+          })}
         />
+        {errors.content && (
+          <ErrorMessage>{errors.content.message}</ErrorMessage>
+        )}
       </div>
       <input
         type="submit"
