bf6b0dae9284a7f6ac3234550e2739ada72bd15c
notlim_dev <elmotigre6@gmail.com>
2024-11-10 13:53:37 -0500

    Enviamos los datos via props de las notas

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -96,7 +96,7 @@ export default function TaskModalDetails() {
                   {data.completedBy.length ? (
                     <>
 
-                      <p className="text-lg text-slate-500 mb-2">Historial de cambios</p>
+                      <p className="font-bold text-2xl text-slate-600 my-5">Historial de cambios</p>
                       <ul className="list-decimal">
                         {data.completedBy.map((activityLog) => (
                           <li key={activityLog._id}>
@@ -120,7 +120,9 @@ export default function TaskModalDetails() {
                       ))}
                     </select>
                   </div>
-                  <NotesPanel />
+                  <NotesPanel
+                    notes={data.notes}
+                  />
                 </DialogPanel>
               </TransitionChild>
             </div>
