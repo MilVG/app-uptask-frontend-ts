62e06fbcbc8c8438ced849e14dd84270981d60e9
notlim_dev <elmotigre6@gmail.com>
2024-11-11 01:56:22 -0500

    Estableciendo el evento de posicion al momento de arrastrar el elemento.

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,4 +1,4 @@
-import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core"
+import { DndContext, DragEndEvent, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core"
 import { Task } from "@/types/index"
 import TaskCard from "./TaskCard"
 import { statusTraslations } from "@/locales/es"
@@ -47,12 +47,20 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
     currentGroup = [...currentGroup, task]
     return { ...acc, [task.status]: currentGroup };
   }, initialStatusGroups);
+
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { over, active } = e
+    if (over && over.id) {
+      console.log('Valido...');
+    }
+  }
   return (
     <>
       <h2 className="text-5xl font-black my-10">Tareas</h2>
 
       <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-        <DndContext sensors={sensors}>
+        <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
           {Object.entries(groupedTasks).map(([status, tasks]) => (
             <div key={status} className='min-w-[301px] 2xl:min-w-0 2xl:w-1/5'>
               <h3
@@ -60,7 +68,7 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
                 bg-white p-3 border-t-8 ${statusStyles[status]}`}
               >{statusTraslations[status]}</h3>
 
-              <DropTask />
+              <DropTask status={status} />
               <ul className='mt-5 space-y-5'>
                 {tasks.length === 0 ? (
                   <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
