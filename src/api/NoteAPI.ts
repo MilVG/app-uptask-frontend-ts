aba9964e62d0ef0524aecdb2360591e87984a575
notlim_dev <elmotigre6@gmail.com>
2024-11-10 21:22:33 -0500

    Estableciendo el servicio de eliminar una nota con axios

--- src/api/NoteAPI.ts
+++ src/api/NoteAPI.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from "axios";
-import { NoteFormData, Project, Task } from "../types";
+import { Note, NoteFormData, Project, Task } from "../types";
 import api from "@/lib/axios";
 
 type NoteAPIType = {
   formData: NoteFormData
   projectId: Project['_id']
   taskId: Task['_id']
+  noteId: Note['_id']
 }
 export async function createNote({ projectId, taskId, formData }: Pick<NoteAPIType, 'projectId' | 'taskId' | 'formData'>) {
   try {
@@ -17,6 +18,19 @@ export async function createNote({ projectId, taskId, formData }: Pick<NoteAPITy
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
 
+    }
+  }
+}
+
+export async function deleteNote({ projectId, taskId, noteId }: Pick<NoteAPIType, 'projectId' | 'taskId' | 'noteId'>) {
+  try {
+
+    const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`
+    const { data } = await api.delete(url)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
     }
   }
 }
