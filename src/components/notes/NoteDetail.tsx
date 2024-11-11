f5b19a8811ced49fb5dfa7ca12ed3a68b6309a7e
notlim_dev <elmotigre6@gmail.com>
2024-11-10 20:26:24 -0500

    Asignamos los datosa cada elemento d ela inerfaz

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,6 +1,19 @@
-export default function NoteDetail() {
+import { Note } from "@/types/index"
+import { formatDate } from "@/utils/utils"
+
+type NoteDetailProps = {
+  note: Note
+}
+export default function NoteDetail({ note }: NoteDetailProps) {
   return (
-    <div>NoteDetail</div>
+    <div className="p-3 flex justify-between items-center">
+      <p>
+        {note.content} por: <span className="font-bold">{note.createdBy.name}</span>
+      </p>
+      <p className="text-xs text-slate-500">
+        {formatDate(note.createdAt)}
+      </p>
+    </div>
   )
 }
 
