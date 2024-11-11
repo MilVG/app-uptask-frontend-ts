9a1db6856f3b9d9efcdea0a475e9cfd573fd9318
notlim_dev <elmotigre6@gmail.com>
2024-11-11 13:07:02 -0500

    modificamos el servicio de obtener el projecto especifico

--- src/views/ProjectDetailsView.tsx
+++ src/views/ProjectDetailsView.tsx
@@ -1,4 +1,4 @@
-import { getProjectById } from "@/api/ProjectAPI"
+import { getFullProject } from "@/api/ProjectAPI"
 import AddTaskModal from "@/components/tasks/AddTaskModal"
 import EditTaskData from "@/components/tasks/EditTaskData"
 import TaskList from "@/components/tasks/TaskList"
@@ -19,7 +19,7 @@ export default function ProjectDetailsView() {
 
   const { data, isError, isLoading } = useQuery({
     queryKey: ['project', projectId],
-    queryFn: () => getProjectById(projectId),
+    queryFn: () => getFullProject(projectId),
     retry: false
   })
 
